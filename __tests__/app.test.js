2b05f834ed5d0d6c79aa6d1f844ef61839dbce86
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-02-19 11:54:57 +0000

    GET /api test added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,13 +7,30 @@ const testData = require("../db/data/test-data/index");
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
+describe("/api", () => {
+  test("GET:200 responds with an object describing all available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const { endpoints } = body;
+        for (const key in endpoints) {
+          expect(endpoints[key]).toHaveProperty("description");
+          expect(endpoints[key]).toHaveProperty("queries");
+          expect(endpoints[key]).toHaveProperty("reqBodyFormat");
+          expect(endpoints[key]).toHaveProperty("exampleResponse");
+        }
+      });
+  });
+});
+
 describe("/api/topics", () => {
   test("GET:200 responds with an array of topic objects", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
       .then(({ body }) => {
-        const topics = body.topics;
+        const { topics } = body;
         expect(topics.length).toBe(3);
         topics.forEach((topic) => {
           expect(typeof topic.description).toBe("string");

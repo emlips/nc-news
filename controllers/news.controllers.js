30377a2d826f65dff8c83402188bba57d14531a4
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-02-19 16:17:15 +0000

    getCommentsByArticleId added

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -3,6 +3,7 @@ const {
   selectEndpoints,
   selectArticleById,
   selectArticles,
+  selectCommentsByArticleId,
 } = require("../models/news.models");
 
 exports.getEndpoints = (req, res, next) => {
@@ -25,10 +26,19 @@ exports.getArticles = (req, res, next) => {
 };
 
 exports.getArticleById = (req, res, next) => {
-    const { article_id } = req.params
+  const { article_id } = req.params;
   selectArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  selectCommentsByArticleId(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
     })
     .catch(next);
 };

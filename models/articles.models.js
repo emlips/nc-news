faccac73d466486c8adc2841dde7207a9705d65a
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-02-22 10:00:35 +0000

    getArticles comment_count refactored

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,9 +1,8 @@
 const db = require("../db/connection");
-const { createCommentRef } = require("../utils");
 
 exports.selectArticles = (topic, sort_by = "created_at", order = "desc") => {
   const queryValues = [];
-  let articlesQuery = `SELECT * FROM articles`;
+  let articlesQuery = `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id`;
   const validSortBy = ["article_id", "title", "author", "created_at", "votes"];
   const validOrder = ["asc", "desc"];
 
@@ -19,55 +18,41 @@ exports.selectArticles = (topic, sort_by = "created_at", order = "desc") => {
     queryValues.push(topic);
   }
 
-  articlesQuery += ` ORDER BY ${sort_by} ${order}`;
+  articlesQuery += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order}`;
 
   const articles = db.query(articlesQuery, queryValues);
   const topicCheck = db.query(`SELECT slug FROM topics WHERE slug = $1`, [
     topic,
   ]);
 
-  return Promise.all([articles, createCommentRef(), topicCheck]).then(
-    ([articles, commentRef, topics]) => {
-      if (topic) {
-        if (!topics.rows[0]) {
-          return Promise.reject({
-            status: 404,
-            msg: "topic not found",
-          });
-        }
+  return Promise.all([articles, topicCheck]).then(([articles, topics]) => {
+    if (topic) {
+      if (!topics.rows[0]) {
+        return Promise.reject({
+          status: 404,
+          msg: "topic not found",
+        });
       }
-
-      articles.rows.forEach((article) => {
-        delete article.body;
-        if (!commentRef[article.article_id]) {
-          article.comment_count = 0;
-        } else {
-          article.comment_count = commentRef[article.article_id];
-        }
-      });
-
-      return articles.rows;
     }
-  );
+    return articles.rows;
+  });
 };
 
 exports.selectArticleById = (id) => {
-  const article = db.query(`SELECT * FROM articles WHERE article_id = $1`, [
-    id,
-  ]);
-  return Promise.all([article, createCommentRef()]).then(
-    ([{ rows }, commentRef]) => {
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      [id]
+    )
+    .then(({ rows }) => {
       if (!rows[0]) {
         return Promise.reject({
           status: 404,
           msg: "article does not exist",
         });
       }
-      const article = rows[0];
-      article.comment_count = Number(commentRef[article.article_id]);
-      return article;
-    }
-  );
+      return rows[0];
+    });
 };
 
 exports.updateArticleById = (id, votesUpdate, votes) => {

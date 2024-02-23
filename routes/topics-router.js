4df29d2890685557df423e6b16dc3fe3b3ce3de9
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-02-23 14:21:29 +0000

    postTopic feature added

--- routes/topics-router.js
+++ routes/topics-router.js
@@ -1,9 +1,10 @@
-const { getTopics } = require("../controllers/topics.controllers");
+const { getTopics, postTopic } = require("../controllers/topics.controllers");
 
 const topicsRouter = require("express").Router();
 
 topicsRouter
   .route("/")
-  .get(getTopics);
+  .get(getTopics)
+  .post(postTopic);
 
 module.exports = topicsRouter;

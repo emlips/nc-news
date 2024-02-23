fce606561524b60cdaf8bd653a2921e7709b9cee
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-02-23 10:51:57 +0000

    models refactored with introduction of routers

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,30 +1,5 @@
 const db = require("../db/connection");
 
-exports.selectCommentsByArticleId = (id) => {
-  return db
-    .query(
-      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at ASC`,
-      [id]
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
-};
-
-exports.insertCommentByArticleId = (id, username, body) => {
-  return db
-    .query(
-      `INSERT INTO comments 
-      (body, article_id, author)
-      VALUES
-      ($1, $2, $3) RETURNING *`,
-      [body, id, username]
-    )
-    .then(({ rows }) => {
-      return rows[0].body;
-    });
-};
-
 exports.removeCommentById = (id) => {
   return db
     .query(`SELECT * FROM comments WHERE comment_id = $1`, [id])
